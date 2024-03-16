ab99b1b183e91c56e35c2e1d0b78268cf7a44e0c
Rohit Dhamale <rohitdhamale05@gmail.com>
2024-03-16 23:27:35 +0530

    User  schema commit

--- models/user.js
+++ models/user.js
@@ -3,12 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-// Search for passport ;
-// passport-local-mongoose, it is a tool which already set iin builts code for user login 
-// it's usefull for login and password and has inbuilt hash functions and salts
 
-// passort automatically save a user name if we don't provide it
-const doctorSchema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         required: true,
@@ -22,14 +18,28 @@ const doctorSchema = new Schema({
         type: String,
 
     },
-  
+    type: {
+        type: String,
+
+    },
     appointments: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Appointment",
         }
     ],
-    
+    patients: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    doctors: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
     
 });
 
