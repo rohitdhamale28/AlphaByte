5b3bf43b19615bc6b6a01ca1118d32ad9e96c298
Rohit Dhamale <rohitdhamale05@gmail.com>
2024-03-16 23:32:22 +0530

    app commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");//for express sessions, But it only for local projects and not production
+const MongoStore = require("connect-mongo");//this is used for production enviornment
+const flash = require("connect-flash");
+const wrapAsync = require("./utils/wrapAsync.js");
+
+
+const passport = require("passport");
+const localStratergy = require("passport-local");
+const User = require("./models/user.js");
+const Blog = require("./models/blog.js");
+const StartUp = require("./models/startup.js");
 const Appointment = require(".models/appointment.js");
 
 app.set("view engine", "html");
@@ -13,7 +27,7 @@ main().then(() => {
 }).catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://localhost:27017/appointment");
+    await mongoose.connect("mongodb://localhost:27017/healthcare");
 }
 
 
@@ -30,6 +44,6 @@ app.post("/appointment", async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log("Server is running on port 3000");
 });
